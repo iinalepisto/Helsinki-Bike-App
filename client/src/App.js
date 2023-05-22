9daf9d1a2ae21613f49b49bcb3b442e3fb7f152d
Iina <AC8451@student.jamk.fi>
2023-05-22 14:34:52 +0300

    Routing pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import { Route, Routes } from "react-router-dom";
-import Navigation from './components/navigation/Navigation.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './components/navigation/Navigation';
+import Home from './pages/home/Home';
+import Journeys from './pages/journeys/Journeys';
+import Stations from './pages/stations/Stations';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
-
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/journeys" element={<Journeys />}></Route>
+        <Route path="/stations" element={<Stations />}></Route>
+      </Routes>
     </div>
   );
 }
