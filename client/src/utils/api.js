9557eef70097bc6ed8c9e293bd24a4b05a21fc55
Iina <AC8451@student.jamk.fi>
2023-05-23 11:26:32 +0300

    Fetching stations data

---
+++ client/src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:8000"
+})
+
+export const fetchStations = async (page, limit, sortBy, sortByOrder) => {
+    try {
+        const res = await api.get(`/api/stations?page=${page - 1}&limit=${limit}&sortBy=${sortBy}&sortOrder=${sortByOrder}`);
+        return res.data;
+    } catch (error) {
+        throw new Error("Error while fetching stations data");
+    }
+}
+
+export const fetchStation = async (id) => {
+    try {
+        const res = await api.get(`/api/stations/${id}`);
+        return res.data;
+    } catch (error) {
+        throw new Error("Error while fetching station data");
+    }
+}
