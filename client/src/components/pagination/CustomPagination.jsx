7f495ba13cc443155cafd6b4a6c0d9c6881d4243
Iina <AC8451@student.jamk.fi>
2023-05-23 12:56:29 +0300

    Changes to handlePageChange using event and value

--- client/src/components/pagination/CustomPagination.jsx
+++ client/src/components/pagination/CustomPagination.jsx
@@ -5,16 +5,16 @@ import "./CustomPagination.css";
 const CustomPagination = ({ page, count, limit, setPage }) => {
     const totalPages = Math.ceil(count / limit);
 
-    const handlePageChange = (value) => {
+    const handlePageChange = (event, value) => {
         setPage(value);
         console.log(value);
-        console.log(page);
     };
 
     return (
         <div>
-            <Box classname="pagiBox">
-                <Pagination className='pagination'
+            <Box className="pagiBox">
+                <Pagination
+                    className='pagination'
                     count={totalPages}
                     page={page}
                     onChange={handlePageChange}
