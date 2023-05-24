8eaf08f68296eab0f6371b5bcf9e84e7d76665ff
Iina <AC8451@student.jamk.fi>
2023-05-24 09:48:16 +0300

    Journey infocard basic setup

--- client/src/components/journeyInfo/JourneyInfo.jsx
+++ client/src/components/journeyInfo/JourneyInfo.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./JourneyInfo.css";
+import { calculateDistance, calculateDuration } from '../../utils/journeyUtils';
 
-const JourneyInfo = (journey) => {
+const JourneyInfo = ({ journey }) => {
     return (
-        <div>
-            {journey.departureStationName}
-            {journey.returnStationName}
+        <div className='infoCard'>
+            <h1>Matka</h1>
+            <p>Matkan lähtöasema {journey.departureStationName}</p>
+            <p>Matkan pääteasema {journey.returnStationName}</p>
+            <p>Matkan pituus {calculateDistance(journey.coveredDistanceM)} km</p>
+            <p>Matkan kesto {calculateDuration(journey.durationSec).minutes} min {calculateDuration(journey.durationSec).seconds} s</p>
         </div>
     )
 }
